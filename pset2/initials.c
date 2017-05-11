518e02db709afd034fa48d3ad1f1b093b2c2a5db
Tod L <ubuntu@b6b4cb6241ef>
2017-05-11 18:05:29 +0000

    Initialize problem set 2.

---
+++ pset2/initials.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main (void)
+{
+    string s = get_string();
+    
+    if (s != NULL)
+    {
+        if (s[0] != ' ')
+        {
+            printf ("%c", toupper(s[0]));
+        }
+        
+        for (int i = 0, n = strlen(s); i < n - 1; i++)
+        {
+            if (s[i] == ' ' && s[i+1] != ' ')
+            {
+                printf ("%c", toupper(s[i+1]));
+            }
+        }
+    }
+    printf ("\n");
+}
