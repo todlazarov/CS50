3bce8fb6c46c7db1da8a0271b288323452f7c8c8
Tod L <ubuntu@b6b4cb6241ef>
2017-05-11 18:09:36 +0000

    Add comments to initials.c

--- pset2/initials.c
+++ pset2/initials.c
@@ -1,3 +1,9 @@
+// Implement a program that, given a person’s name, prints a person’s initials, per the below.
+
+// $ ./initials
+// Regulus Arcturus Black
+// RAB
+
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
@@ -7,20 +13,26 @@ int main (void)
 {
     string s = get_string();
     
+    // Check if sting is null
     if (s != NULL)
     {
+        // If the first character in the string is not space, print out the first initial
         if (s[0] != ' ')
         {
             printf ("%c", toupper(s[0]));
         }
         
+        // Iterate through the string
         for (int i = 0, n = strlen(s); i < n - 1; i++)
         {
+            // Check for the next initial
             if (s[i] == ' ' && s[i+1] != ' ')
             {
                 printf ("%c", toupper(s[i+1]));
             }
         }
     }
+    
+    // End with a new line, as per spec
     printf ("\n");
 }
