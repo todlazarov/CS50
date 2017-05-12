8e32cfd2192a1c2a4f79c3a1e85d3232657458a5
Tod L <ubuntu@b6b4cb6241ef>
2017-05-12 19:35:51 +0000

    Complete vigenere cypher

---
+++ pset2/vigenere/vigenere.c
@@ -0,0 +1,92 @@
+#include <cs50.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, string argv[])
+{
+    // Check to ensure there are only 2 arguments (the name of the file and the shift)
+    if (argc != 2)
+    {
+        printf ("Usage: ./vigenere k\n");
+        return 1;
+    }
+    
+    // Check each key to ensure they are all letters
+    for (int j = 0, m = strlen(argv[1]); j < m; j++)
+    {
+        if (!isalpha(argv[1][j]))
+        {
+            printf ("Usage: ./vigenere k");
+            return 1;
+        }
+    }
+    
+    string shift_word = argv[1];
+    char shift_key;
+    int len = strlen(argv[1]);
+    int k = 0;
+    int i = 0;
+    int l = 0;
+    //int o = 0;
+    
+    // Ask user and store the plain text
+    printf ("plaintext: ");
+    string p = get_string();
+    int p_len = strlen(p);
+    
+    printf ("ciphertext: ");
+    
+    // Iterate through input
+    while (true)
+    {
+        // Check if the char is a letter
+        if (isalpha(p[l]))
+        {
+            // Shift the char if its lower case
+            if (islower(p[l]))
+            {
+                shift_key = shift_word[i % len];
+                if (islower(shift_key))
+                {
+                    k = shift_key - 97;
+                }
+                else{
+                    k = shift_key - 65;
+                }
+                printf ("%c", 'a' + (p[l] + k - 'a') % 26);
+                i++;
+            } 
+            
+            // Shift the char if its upper case
+            if (isupper(p[l]))
+            {
+                shift_key = shift_word[i % len];
+                if (islower(shift_key))
+                {
+                    k = shift_key - 97;
+                }
+                else{
+                    k = shift_key - 65;
+                }
+                printf ("%c", 'A' + (p[l] + k - 'A') % 26);
+                i++;
+            }
+        } 
+        // Print characters that are not letters as is
+        else
+        {
+            printf ("%c", p[l]);
+        }
+        
+        l++;
+        
+        if (l >= p_len)
+        {
+            printf ("\n");
+            return 0;
+            break;
+        }
+    }
+}
