bf5cf39c922510c56915505abe6bc3883606f349
Tod L <tod.lazarov1@gmail.com>
2017-05-15 23:13:13 +0000

    Comment helper.c

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -19,22 +19,28 @@ bool search(int value, int values[], int n)
     int left = 0;
     int right = len;
     
+    // Return false if the size of the array given is a negative integer, as per spec
     if (n <= 0) {
         return false;
     }
     
+    
+    // Main binary search loop
     while (len > 0)
     {
+        // Check if value is found
         if (value == values[middle])
         {
             return true;
         }
+        // Fork splitting the array in half, this one looks at the higher half
         else if (value > values[middle])
         {
             left = middle + 1;
             middle = (right + left) / 2;
             len = right - left + 1;
         }
+        // Fork splitting the array in half, this one looks at the lower half
         else if (value < values[middle])
         {
             right = middle - 1;
@@ -60,6 +66,7 @@ void sort(int values[], int n)
             max = values[i];
         }
     }
+    
     // Declare variables
     int * count_arr = calloc(max, sizeof(int));
     int * arr2 = calloc(max, sizeof(int));
@@ -72,24 +79,28 @@ void sort(int values[], int n)
         count_arr[i] = 0;
     }
     
+    // Count a total for each number in the existing array
     for (int i = 0; i < n; i++)
     {
         count_arr[values[i]] = count_arr[values[i]] + 1;
     }
     
+    // Add up all of the counts in the counter array
     for (int i = 0; i < n; i++)
     {
         counter += count_arr[i+1];
         count_arr[i+1] = counter;
     }
     
+    // Reorganize in ordered form the final array
     for (int i = n; i >= 0; i--)
     {
         idx = count_arr[values[i]];
         arr2[idx] = values[i];
     }
     
+    // Set the input array equal to the final ordered array
     values = arr2;
     
-    return ;
+    return;
 }
